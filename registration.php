53f510ca3d145991544012a0b88e190929ea5fab
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-01-29 21:37:40 +0100

    Add registration.php file

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Przemek\ProductsExporter;
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'YellowCard_ProductsExporter', __DIR__);
