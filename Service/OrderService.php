9534a348d8e22fb8f1e21aeb79e5ff990b68f8ac
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-02-16 17:39:56 +0100

    Add order servie to get all orders with specific ids

--- Service/OrderService.php
+++ Service/OrderService.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace YellowCard\ProductsExporter\Service;
 
 use Magento\Sales\Api\Data\OrderInterface;
+use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Sales\Model\ResourceModel\Order\Collection;
 use Magento\Sales\Model\ResourceModel\Order\CollectionFactory;
 
@@ -16,21 +17,37 @@ class OrderService
      */
     public function __construct(
         private CollectionFactory $collectionFactory,
-        private StatusService $statusService
+        private StatusService $statusService,
+        private OrderRepositoryInterface $orderRepository,
     ) {
     }
 
     /**
-     * Returns collection of orders with specific status provided in config by user
+     * Returns collection of orders IDS with specific status provided in config by user
      *
      * @return Collection
      */
-    public function getOrders(): Collection
+    public function getOrderIds(): Collection
     {
         $collection = $this->collectionFactory->create();
         $collection->addFieldToFilter('status', $this->statusService->getStatus());
         $collection->addAttributeToSelect(OrderInterface::INCREMENT_ID);
 
         return $collection;
+    }
+
+    /**
+     * Loads all orders with specific status, found by ids provided from method above
+     *
+     * @return array
+     */
+    public function getOrders(): array
+    {
+        $orders = [];
+        foreach ($this->getOrderIds() as $orderId) {
+            $orders[] = $this->orderRepository->get($orderId->getIncrementId());
+        }
+
+        return $orders;
     }
 }
