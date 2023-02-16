77f95fb77cf315960c020ef40dd9e235bce41652
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-02-16 19:55:33 +0100

    Update main controller action class to be more self descriptive

---
+++ Controller/Adminhtml/Index/Export.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YellowCard\ProductsExporter\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use YellowCard\ProductsExporter\Service\ExportService;
+
+class Export extends Action implements HttpGetActionInterface
+{
+
+    /**
+     * @param ExportService $exportService
+     * @param Context       $context
+     */
+    public function __construct(
+        private ExportService $exportService,
+        Context $context
+    ) {
+        parent::__construct($context);
+    }
+
+    /**
+     * @return void
+     */
+    public function execute(): void
+    {
+        $this->exportService->createExport();
+    }
+}
