2da6e0dd54b15628db52c5da2520c1363d6b0449
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-02-19 23:33:37 +0100

    Dispatch events when export is created, or something went wrong

--- Service/ExportService.php
+++ Service/ExportService.php
@@ -4,15 +4,20 @@ declare(strict_types=1);
 
 namespace YellowCard\ProductsExporter\Service;
 
+use Magento\Framework\Event\ManagerInterface;
 use Psr\Log\LoggerInterface;
 
 class ExportService
 {
     /**
-     * @param ProductService  $productService
-     * @param LoggerInterface $logger
+     * @param ProductService   $productService
+     * @param LoggerInterface  $logger
+     * @param ManagerInterface $eventManager
      */
-    public function __construct(private ProductService $productService, private LoggerInterface $logger)
+    public function __construct(
+        private ProductService $productService,
+        private LoggerInterface $logger,
+        private ManagerInterface $eventManager)
     {
     }
 
@@ -31,8 +36,10 @@ class ExportService
                         . " " . $item->getOrderId() . '</br>';
                 }
             }
+            $this->eventManager->dispatch('export_success');
         } catch (\Exception $exception) {
             $this->logger->critical($exception->getMessage());
+            $this->eventManager->dispatch('export_failed');
         }
 
     }
