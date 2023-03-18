930da08a02b2828b529d23360096044634264484
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-03-18 15:00:44 +0100

    Extend order service to save into db nubers of orders from which te export was created

--- Service/OrderService.php
+++ Service/OrderService.php
@@ -8,6 +8,10 @@ use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Api\OrderRepositoryInterface;
 use Magento\Sales\Model\ResourceModel\Order\Collection;
 use Magento\Sales\Model\ResourceModel\Order\CollectionFactory;
+use Psr\Log\LoggerInterface;
+use YellowCard\ProductsExporter\Enum\LoggerMessages;
+use YellowCard\ProductsExporter\Model\ExportedOrdersFactory;
+use YellowCard\ProductsExporter\Model\ResourceModel\ExportedOrders as ExportedOrdersResource;
 
 class OrderService
 {
@@ -15,11 +19,17 @@ class OrderService
      * @param CollectionFactory        $collectionFactory
      * @param StatusService            $statusService
      * @param OrderRepositoryInterface $orderRepository
+     * @param ExportedOrdersFactory    $exportedOrdersFactory
+     * @param ExportedOrdersResource   $exportedOrdersResource
+     * @param LoggerInterface          $logger
      */
     public function __construct(
         private CollectionFactory $collectionFactory,
         private StatusService $statusService,
         private OrderRepositoryInterface $orderRepository,
+        private ExportedOrdersFactory $exportedOrdersFactory,
+        private ExportedOrdersResource $exportedOrdersResource,
+        private LoggerInterface $logger
     ) {
     }
 
@@ -45,10 +55,47 @@ class OrderService
     public function getOrders(): array
     {
         $orders = [];
+        $orderIds = [];
         foreach ($this->getOrderIds() as $orderId) {
             $orders[] = $this->orderRepository->get($orderId->getIncrementId());
+            $orderIds[] = $orderId->getIncrementId();
         }
+        $this->saveOrderIds($orderIds);
 
         return $orders;
+    }
+
+    /**
+     * Saves string of orders from which the export was generated
+     *
+     * @param array $orderIds
+     *
+     * @return void
+     */
+    private function saveOrderIds(array $orderIds): void
+    {
+        $stringOfOrderNumbers = $this->convertOrderArrayToString($orderIds);
+
+        $exportedOrders = $this->exportedOrdersFactory->create();
+        $exportedOrders->setOrders($stringOfOrderNumbers);
+        $exportedOrders->setRaportId(1);
+
+        try{
+            $this->exportedOrdersResource->save($exportedOrders);
+        } catch (\Exception $exception) {
+            $this->logger->critical(LoggerMessages::DB_FAILED->value. " : " .$exception->getMessage());
+        }
+    }
+
+    /**
+     * Converts array of given orders to a string, to store it in this form in the database
+     *
+     * @param array $orderIds
+     *
+     * @return string
+     */
+    private function convertOrderArrayToString(array $orderIds): string
+    {
+        return implode(",", $orderIds);
     }
 }
