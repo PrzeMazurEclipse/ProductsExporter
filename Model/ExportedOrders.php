3747a6981cac0ffdc1425595c4fb48cf889458f1
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-03-18 14:52:49 +0100

    Add getters and setters to ExportedOrders model

--- Model/ExportedOrders.php
+++ Model/ExportedOrders.php
@@ -9,8 +9,43 @@ use YellowCard\ProductsExporter\Model\ResourceModel\ExportedOrders as ExportedOr
 
 class ExportedOrders extends AbstractModel
 {
+    const ORDERS = 'orders';
+    const RAPORT_ID = 'raport_id';
+
     protected function _construct()
     {
         $this->_init(ExportedOrdersResource::class);
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrders(): string
+    {
+        return $this->getData(self::ORDERS);
+    }
+
+    /**
+     * @param string $Orders
+     */
+    public function setOrders(string $Orders): void
+    {
+        $this->setData(self::ORDERS, $Orders);
+    }
+
+    /**
+     * @return int
+     */
+    public function getRaportId(): int
+    {
+        return $this->getData(self::RAPORT_ID);
+    }
+
+    /**
+     * @param int $raport_id
+     */
+    public function setRaportId(int $raport_id): void
+    {
+        $this->setData(self::RAPORT_ID, $raport_id);
     }
 }
