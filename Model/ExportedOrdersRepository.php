07e2c3e78aabfbc8c6254bc1dd7781dd47e703f5
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-03-18 22:32:53 +0100

    FIll method to retrieve last entity with exported order numbers, add doc-blocks

--- Model/ExportedOrdersRepository.php
+++ Model/ExportedOrdersRepository.php
@@ -9,15 +9,29 @@ use YellowCard\ProductsExporter\Api\Data\ExportedOrdersInterface;
 use YellowCard\ProductsExporter\Api\ExportedOrdersRepositoryInterface;
 use YellowCard\ProductsExporter\Enum\LoggerMessages;
 use YellowCard\ProductsExporter\Model\ResourceModel\ExportedOrders as ExportedOrdersResource;
+use YellowCard\ProductsExporter\Model\ResourceModel\ExportedOrders\CollectionFactory;
 
 class ExportedOrdersRepository implements ExportedOrdersRepositoryInterface
 {
+    /**
+     * @param ExportedOrdersResource $exportedOrdersResource
+     * @param LoggerInterface        $logger
+     * @param CollectionFactory      $exportedOrdersCollectionFactory
+     */
     public function __construct(
         private ExportedOrdersResource $exportedOrdersResource,
-        private LoggerInterface $logger
+        private LoggerInterface $logger,
+        private CollectionFactory $exportedOrdersCollectionFactory
     ) {
     }
 
+    /**
+     * Save to db an entity with numbers of orders from which export was created
+     *
+     * @param ExportedOrdersInterface $exportedOrders
+     *
+     * @return ExportedOrdersInterface
+     */
     public function save(ExportedOrdersInterface $exportedOrders
     ): ExportedOrdersInterface {
         try{
@@ -29,8 +43,17 @@ class ExportedOrdersRepository implements ExportedOrdersRepositoryInterface
         return $exportedOrders;
     }
 
+    /**
+     * Return last created entity with exported order numbers
+     *
+     * @return ExportedOrdersInterface
+     */
     public function getLastExportedOrders(): ExportedOrdersInterface
     {
-        // TODO: Implement getLastExportedOrders() method.
+        $lastExportedOrdersCollection = $this->exportedOrdersCollectionFactory->create();
+        $lastExportedOrdersCollection->addOrder('id', 'DESC');
+        $lastExportedOrdersEntity = $lastExportedOrdersCollection->getFirstItem();
+
+        return $lastExportedOrdersEntity;
     }
 }
