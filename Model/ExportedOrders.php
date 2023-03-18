7507cba744f3da96ea7be0a2e8c9ed00c7caabd7
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-03-18 11:55:33 +0100

    Add exportedOrders model

---
+++ Model/ExportedOrders.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YellowCard\ProductsExporter\Model;
+
+use Magento\Framework\Model\AbstractModel;
+use YellowCard\ProductsExporter\Model\ResourceModel\ExportedOrders as ExportedOrdersResource;
+
+class ExportedOrders extends AbstractModel
+{
+    protected function _construct()
+    {
+        $this->_init(ExportedOrdersResource::class);
+    }
+}
