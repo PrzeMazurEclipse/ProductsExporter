722f2012f604072007db89e2859d6e90c842432d
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-03-19 18:25:51 +0100

    Implement interface into export model, and getters and setters

--- Model/Export.php
+++ Model/Export.php
@@ -5,13 +5,39 @@ declare(strict_types=1);
 namespace YellowCard\ProductsExporter\Model;
 
 use Magento\Framework\Model\AbstractModel;
+use YellowCard\ProductsExporter\Api\Data\ExportInterface;
 use YellowCard\ProductsExporter\Model\ResourceModel\Export as ExportResource;
 
-class Export extends AbstractModel
+class Export extends AbstractModel implements ExportInterface
 {
 
     protected function _construct()
     {
         $this->_init(ExportResource::class);
+    }
+
+    public function getTitle(): string
+    {
+       return $this->getData(self::TITLE);
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->setData(self::TITLE, $title);
+    }
+
+    public function getStatus(): string
+    {
+        return $this->getData(self::STATUS);
+    }
+
+    public function setStatus(string $status)
+    {
+        $this->setData(self::STATUS, $status);
+    }
+
+    public function getCreatedAt(): string
+    {
+        return $this->getData(self::CREATED_AT);
     }
 }
