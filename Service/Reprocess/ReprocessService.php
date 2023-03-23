6a9fcd4285a57e8cf91ad369d58fcdccb4fedbfb
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-03-23 21:03:10 +0100

    CLeaning reprocess service

--- Service/Reprocess/ReprocessService.php
+++ Service/Reprocess/ReprocessService.php
@@ -6,12 +6,23 @@ namespace YellowCard\ProductsExporter\Service\Reprocess;
 
 class ReprocessService
 {
+    /**
+     * @param LoadOrdersService $loadOrdersService
+     */
     public function __construct(
-        private readonly LoadOrdersService $loadOrderService
+        private readonly LoadOrdersService $loadOrdersService
     ) {
     }
-    public function Reprocess(array $specificRaport)
+
+    /**
+     * Reprocess export of given order numbers from specific raport
+     *
+     * @param array $specificRaport
+     *
+     * @return void
+     */
+    public function reprocess(array $specificRaport)
     {
-        $orders = $this->loadOrderService->execute($specificRaport);
+        $orders = $this->loadOrdersService->execute($specificRaport);
     }
 }
