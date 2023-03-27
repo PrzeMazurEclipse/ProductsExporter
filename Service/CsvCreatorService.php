fd07b6f2343ac8b21a0763b2e3b3069a86564c32
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-03-27 22:48:16 +0200

    Add service which is responsible for creating csv file with purchased products

---
+++ Service/CsvCreatorService.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YellowCard\ProductsExporter\Service;
+
+class CsvCreatorService
+{
+    const PATH = 'exportedProducts/';
+    const FILE_NAME = 'Exported_Products';
+    const EXTENSION = '.csv';
+
+    const SKU = 'sku';
+    const QTY = 'qty_ordered';
+    const ORDER_ID = 'order_id';
+
+    private array $csv_column_names = ['SKU', 'QUANTITY', 'ORDER_ID'];
+
+    /**
+     * Creates a csv file from provided purchased products from orders. Will be stored in above path.
+     *
+     * @param array $exportedProducts
+     *
+     * @return void
+     */
+    public function createCsvFromGivenExportedProducts(array $exportedProducts): void
+    {
+
+        $arrayOfProducts = $this->getFromArrayOfProductObjectsRequiredInformation($exportedProducts);
+
+        $currentExportName = self::PATH.self::FILE_NAME.$this->setDateForCurrentExport().self::EXTENSION;
+
+        $csvFile = fopen($currentExportName, 'w+');
+
+        //Adds column names to be first row
+        fputcsv($csvFile, $this->csv_column_names);
+
+        foreach ($arrayOfProducts as $row)
+        {
+            fputcsv($csvFile, $row);
+        }
+
+        fclose($csvFile);
+    }
+
+    /**
+     * Return array which in rows contains information about purchased product - sku, qty, order id
+     *
+     * @param array $exportedProducts
+     *
+     * @return array
+     */
+    private function getFromArrayOfProductObjectsRequiredInformation(array $exportedProducts): array
+    {
+        $productsArray = [];
+
+        foreach ($exportedProducts as $product) {
+            foreach ($product as $item) {
+                $productsArray[] = [
+                    $item[self::SKU],
+                    number_format((float)$item[self::QTY], 2),
+                    $item[self::ORDER_ID]];
+            }
+        }
+
+        return $productsArray;
+    }
+
+    /**
+     * Return readable current data string
+     *
+     * @return string
+     */
+    private function setDateForCurrentExport(): string
+    {
+        return date('Y-m-d', time());
+    }
+}
