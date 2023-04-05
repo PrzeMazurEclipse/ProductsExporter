cb402a9b3a8ab23acd84fe32b600b01ac4858917
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-04-05 19:26:43 +0200

    Use csv service to create reproccessed export csv file

--- Service/Reprocess/ReprocessService.php
+++ Service/Reprocess/ReprocessService.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 
 namespace YellowCard\ProductsExporter\Service\Reprocess;
 
+use YellowCard\ProductsExporter\Service\CsvCreatorService;
+
 class ReprocessService
 {
     /**
      * @param LoadOrdersService   $loadOrdersService
      * @param LoadProductsService $loadProductsService
+     * @param CsvCreatorService   $csvCreatorService
      */
     public function __construct(
         private readonly LoadOrdersService $loadOrdersService,
-        private readonly LoadProductsService $loadProductsService
+        private readonly LoadProductsService $loadProductsService,
+        private readonly CsvCreatorService $csvCreatorService
     ) {
     }
 
@@ -27,5 +31,7 @@ class ReprocessService
     {
         $orders = $this->loadOrdersService->execute($specificRaport);
         $products = $this->loadProductsService->execute($orders);
+        $this->csvCreatorService->createCsvFromGivenExportedProducts($products);
+
     }
 }
