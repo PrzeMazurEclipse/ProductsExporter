84ffc59d0b1d534adc226e79a69cd03d4e7d1543
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-04-11 22:37:23 +0200

    Add controller which will handle download csv file with exported products

---
+++ Controller/Adminhtml/Download/Download.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YellowCard\ProductsExporter\Controller\Adminhtml\Download;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+
+class Download extends Action implements HttpGetActionInterface
+{
+    public function execute()
+    {
+    }
+}
