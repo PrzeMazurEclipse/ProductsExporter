782e471733954f2f665b6d27ae075f6767aedf34
PrzeMazurEclipse <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-04-15 22:03:50 +0200

    Add result page when file does not exist or there were some problem with thep process

--- Controller/Adminhtml/Download/Download.php
+++ Controller/Adminhtml/Download/Download.php
@@ -11,23 +11,27 @@ use Magento\Framework\App\Action\HttpGetActionInterface;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\App\Response\Http\FileFactory;
+use Magento\Framework\View\Result\PageFactory;
 use Psr\Log\LoggerInterface;
 
 class Download extends Action implements HttpGetActionInterface
 {
     private const DOWNLOAD_PATH = 'exportedProducts/';
     private const EXTENSION = '.csv';
+    private string $errorMessage;
 
     /**
      * @param FileFactory          $fileFactory
      * @param ScopeConfigInterface $scopeConfig
      * @param LoggerInterface      $logger
+     * @param PageFactory          $pageFactory
      * @param Context              $context
      */
     public function __construct(
         private readonly FileFactory $fileFactory,
         private readonly ScopeConfigInterface $scopeConfig,
         private readonly LoggerInterface $logger,
+        private readonly PageFactory $pageFactory,
         Context $context
     ) {
         parent::__construct($context);
@@ -36,11 +40,17 @@ class Download extends Action implements HttpGetActionInterface
     /**
      * Call private method to download export file
      *
-     * @throws Exception
      */
     public function execute()
     {
-        $this->downloadFile();
+        if(!$this->downloadFile()){
+            $this->messageManager->addErrorMessage(__($this->errorMessage));
+            $resultPage = $this->pageFactory->create();
+            $resultPage->getConfig()->getTitle()->set(__('File not found'));
+
+            return $resultPage;
+        }
+        return true;
     }
 
     /**
@@ -62,9 +72,10 @@ class Download extends Action implements HttpGetActionInterface
     {
         $title =  $this->getRequest()->getParam('title');
         $absoluteFilePath = self::DOWNLOAD_PATH.$title.self::EXTENSION;
-
         try{
             if(!file_exists($absoluteFilePath)) {
+                $this->errorMessage = "File that you want to download does not exist. Please download reprocessed one";
+
                 return false;
             }
             $this->fileFactory->create(
@@ -78,10 +89,10 @@ class Download extends Action implements HttpGetActionInterface
             );
         } catch (Exception $e) {
             $this->logger->error('Something went wrong when downloading export file: %1'. $e->getMessage());
+            $this->errorMessage = "Something went wrong when downloading export file. Check log messages";
 
             return false;
         }
-
         return true;
     }
 }
